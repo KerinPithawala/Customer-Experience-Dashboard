9d96a753224fb6d25c513f6930bfca64ee3382bd
Kerin Pithawala <kerinpithawala7@gmail.com>
2021-07-11 17:53:24 +0530

    Update app.py
    Adding 2 more vizualization

--- app.py
+++ app.py
@@ -1,39 +1,64 @@
-from altair.vegalite.v4.api import value
-from altair.vegalite.v4.schema.channels import Color
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as pz
+import plotly.express as px
 
-st.title("customer review of leading us airlines")
-st.sidebar.title("customer review of leading us airlines")
-st.sidebar.markdown(" sentinment analysis of tweets dashboard")
+import matplotlib.pyplot as plt
 
-data_url=("https://raw.githubusercontent.com/KerinPithawala/Customer-Experience-Dashboard/main/Tweets.csv")
+st.title("Customer Experience Analysis")
+st.sidebar.title("Customer Experience Analysis")
+
+st.markdown("US Airline Tweets ")
+st.sidebar.markdown(" Tweets for US Airlines ")
+
+
+DATA_URL = ("https://raw.githubusercontent.com/KerinPithawala/Customer-Experience-Dashboard/main/Tweets.csv")
 
 @st.cache(persist=True)
 def load_data():
-    data=pd.read_csv(data_url)
-    data['tweet_created']=pd.to_datetime(data['tweet_created'])
+    data = pd.read_csv(DATA_URL)
+    data['tweet_created'] = pd.to_datetime(data['tweet_created'])
     return data
-data=load_data()
 
-st.sidebar.subheader('Have a look at a tweet')
-random_tweet=st.sidebar.radio('Sentiment',('positive','neutral','negative'))
-st.sidebar.markdown(data.query('airline_sentiment== @random_tweet')[['text']].sample(n=1).iat[0,0])
+data = load_data()
 
-st.sidebar.markdown("## Display graphs")
-select=st.sidebar.selectbox('Vizualization type',['Pie Chart','Histogram'],key= '1')
-sentiment_count=data['airline_sentiment'].value_counts()
-sentiment_count=pd.DataFrame({'sentiment':sentiment_count.index,'Tweets':sentiment_count.values })
+st.sidebar.subheader("Show random tweet")
+random_tweet = st.sidebar.radio('Sentiment type', ('positive','negative','neutral'))
+st.sidebar.markdown(data.query('airline_sentiment == @random_tweet')[["text"]].sample(n=1).iat[0,0])
 
-if not st.sidebar.checkbox("Hide",True):
-    st.markdown("Number of tweets by sentiment")
-    if select=='Histogram':
-        fig=pz.bar(sentiment_count,x='Sentiment',y='Tweets',color='Tweets',height= 500 )
+st.sidebar.markdown("### Number of tweets by sentiment type")
+select = st.sidebar.selectbox('Vizualization type', ['Histogram', 'Pie Chart'], key='1')
+sentiment_count = data['airline_sentiment'].value_counts()
+sentiment_count = pd.DataFrame({'Sentiment' :sentiment_count.index, 'Tweets' :sentiment_count.values})
+
+if not st.sidebar.checkbox('Hide', True):
+    st.markdown("### Number of tweets by Sentiment")
+    if select == "Histogram":
+        fig = px.bar(sentiment_count, x='Sentiment', y='Tweets', color = 'Tweets', height= 500)
         st.plotly_chart(fig)
     else:
-        fig=pz.pie(sentiment_count,values='Tweets', names="Sentiment" )
+        fig = px.pie(sentiment_count, values='Tweets', names='Sentiment')
         st.plotly_chart(fig)
+
+st.sidebar.subheader("Filter by time")
+hour = st.sidebar.slider("Hour of day", 0, 23)
+modified_data = data[data['tweet_created'].dt.hour == hour]
+if not st.sidebar.checkbox("Close", True, key='2'):
+    st.markdown("### Tweets location based on the time of the day")
+    st.markdown("%i, tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour+1)%24))
+    st.map(modified_data)
+    if st.sidebar.checkbox("Show raw data", False):
+        st.write(modified_data)
+
+
+st.sidebar.subheader("Filter by airlines")
+choice = st.sidebar.multiselect("Pick airlines", ('US Airways', 'United', 'American', 'Southwest', 'Delta', 'Virgin America'), key = '0')
+
+
+if len(choice) >0:
+    choice_data = data[data.airline.isin(choice)]
+    fig_0 = px.histogram(choice_data, x='airline', y='airline_sentiment', histfunc='count', color='airline_sentiment', facet_col = 'airline_sentiment', labels={'airline_sentiment':'tweets'}, height=600, width=800)
+    st.plotly_chart(fig_0)
+
 
 
